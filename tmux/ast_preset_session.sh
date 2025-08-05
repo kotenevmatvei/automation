5eff754a2f330c40173337023bc39c50c3138a63
Matvei Kotenev <kotenev.matvei@gmail.com>
2025-08-05 14:01:48 +0200

    ast tmux update

--- tmux/ast_preset_session.sh
+++ tmux/ast_preset_session.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SESSION_NAME="ast"
-SLEEP=0.00000000000000001
+SLEEP=0.5
 
 tmux new-session -d -s "$SESSION_NAME" -n "nvim" -c "$HOME/projects/attachment-style/"
 sleep $SLEEP 
@@ -25,5 +25,7 @@ sleep $SLEEP
 tmux send-keys -t "$SESSION_NAME:run_app" "python app.py" C-m
 sleep $SLEEP 
 
+tmux new-window -t "$SESSION_NAME" -n "bash" -c "$HOME/projects/attachment-style/attachment_style/"
+sleep $SLEEP 
 
 tmux attach-session -t "$SESSION_NAME"
